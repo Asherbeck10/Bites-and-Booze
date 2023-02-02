c691a5fc423d83b060afa41ca99476b59d438581
Coni Hinojosa <coni@Conis-MacBook-Pro.local>
2023-02-02 17:43:59 +0000

    finished with search bar and modal

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("search-button").addEventListener("click", function (event) {
+    let ingredient = document.getElementById("search-input").value;
+    event.preventDefault();
+
+});
+
+

1f69473ce0fbcda685bddea0490976df77f4e31e
Tala Ammoun <talaammoun@gmail.com>
2023-02-03 19:56:36 +0000

    fetched image from food API and drinks data from drinks API. Appended everything to the drinks-card divs. Used a for loop to generate 3 random drinks from the drinks array. Tested the drinks array to make sure that all drink names exist in the drinks API, and that all the drinks have corresponding images in the food API.

--- script.js
+++ script.js
@@ -1,24 +1,118 @@
-let drinksImg = $(".drinks-img")
-let drinksText = $(".drinks-text")
-let drinksCard = $(".drinks-card")
+let drinksURL = "https://api.api-ninjas.com/v1/cocktail?name="
+let drinkKey = "Qi651/P1cNZMlzbO7KcHFw==j6GIGW3DJULCUbIq"
+let foodURL = "https://api.edamam.com/api/recipes/v2?type=public&q="
+let foodKey = "&app_key=8655a5154efa47cd96813434d7914422"
+let foodId = "&app_id=630342ea"
+let drinkAPI
+let foodAPI
 
-for (let i = 0; i < result.length; i++) {
-    let drinksDiv = $("<div>")
+//DRINKS API FETCH REQUEST
 
-    drinksImg.attr("src", results[i].images.fixed_height.url)
-    drinksDiv.append(drinkImage)
+let drinksArray = [
+    "Alexander", 
+    "Americano", 
+    "Angel Face", 
+    "Aviation", 
+    "Bacardi", 
+    "Between The Sheets", 
+    "Casino", 
+    "Clover Club", 
+    "Daiquiri", 
+    "Derby",
+    "John Collins", 
+    "Manhattan", 
+    "Monkey Gland", 
+    "Negroni", 
+    "Old Fashioned", 
+    "Paradise", 
+    "Ramos Gin Fizz", 
+    "Rusty Nail", 
+    "Sazerac", 
+    "Screwdriver", 
+    "Sidecar", 
+    "Stinger", 
+    "Tuxedo", 
+    "Whiskey Sour", 
+    "White Lady", 
+    "Bellini", 
+    "Black Russian", 
+    "Bloody Mary", 
+    "Caipirinha", 
+    "Champagne Cocktail", 
+    "Cosmopolitan", 
+    "Cuba Libre", 
+    "French 75",  
+    "Golden Dream", 
+    "Grasshopper", 
+    "Harvey Wallbanger", 
+    "Irish Coffee", 
+    "Kir", 
+    "Long Island Iced Tea", 
+    "Mai Tai", 
+    "Margarita", 
+    "Mimosa", 
+    "Mint Julep", 
+    "Mojito", 
+    "Moscow Mule",  
+    "Rose", 
+    "Sea Breeze", 
+    "Sex On The Beach", 
+    "Singapore Sling", 
+    "Tequila Sunrise", 
+    "Bramble", 
+    "Martini", 
+    "Kamikaze", 
+    "Pisco Sour", 
+    "Vampiro", 
+    "Yellow Bird", 
+    "Daquiri", 
+    "Aperol spritz" 
+]
 
-    let drinksRecipe = results[i].recipe
-    drinksDiv.append(drinksRecipe)
+$(".drink-btn").on("click", function () {
+    $(".drinks-card").empty()
+    for (let x = 0; x < drinksArray.length && x < 3; x++) {
+        let randomDrinkName = Math.floor((Math.random() * drinksArray.length))
+        console.log(randomDrinkName++)
+        let randomDrinkToFetch = drinksArray[randomDrinkName]
+        console.log(randomDrinkToFetch)
 
-    Object.keys($(".drinks-card")).forEach((element) => {
-        if (element < 3) {
-            $($(".drinks-card")[element]).attr("data-index", element)}
-    let drinksIndex = $($(".drinksCard")[element]).attr("data-index")
-    drinksCard[drinksIndex].append(drinksDiv)
+        drinkAPI = drinksURL + randomDrinkToFetch
+        foodAPI = foodURL + randomDrinkToFetch + foodId + foodKey
 
-})
-}
+        Promise.all([
+            fetch(foodAPI),
+            fetch(drinkAPI, { headers: { "X-Api-Key": "Qi651/P1cNZMlzbO7KcHFw==j6GIGW3DJULCUbIq" } })])
+            .then(function (response) {
+                return Promise.all(response.map(function (response) {
+                    return response.json();
+                }));
+            })
+            .then(function (results) {
+                console.log(results)
+                let image = results[0];
+                let drinksData = results[1]
 
-// localStorage.setItem( , )
-// localStorage.getItem( , )
+                $(".drinks-card").css("display", "flex");
+
+                //console.log(drinksData[0].name)
+                let drinkName = $("<p>").text(drinksData[0].name)
+                drinkName.css("text-align", "center")
+
+                let drinkImage = $("<img>")
+                //console.log(image.hits[0].recipe.images.REGULAR.url)
+                drinkImage.attr("src", image.hits[0].recipe.image)
+
+                //console.log(drinksData[0].ingredients)
+                let ingredients = $("<p>").text("Ingredients: " + drinksData[0].ingredients)
+
+                //console.log(drinksData[0].instructions)
+                let instructions = $("<p>").text("Instructions: " + (drinksData[0].instructions))
+
+                //console.log(Math.round(image.hits[0].recipe.calories)
+                let drinkCalories = $("<p>").text("Calories: " + Math.round(image.hits[0].recipe.calories) + "Kcal")
+
+                $(".drinks-card").eq(x).append(drinkName, drinkImage, ingredients, instructions, drinkCalories)
+            })
+    }}
+)
